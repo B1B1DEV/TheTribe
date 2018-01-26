93843d268d537d265c16f5a93cf38d5f0f6f64c6
Olivier <ochabiron@immersivefactory.com>
2018-01-26 23:02:45 +0100

    tribemanager + metas

---
+++ TheTribe/Assets/Scripts/TribeManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TribeManager : MonoBehaviour {
+
+    // private properties
+    int age;
+    int step;
+    int faith;
+
+    [SerializeField]
+    float baseRefusalChance;
+
+    // public Get Methods
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public int GetStep()
+    {
+        return step;
+    }
+
+    public int GetFaith()
+    {
+        return faith;
+    }
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        age = 0;
+        step = 0;
+        faith = 3;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
