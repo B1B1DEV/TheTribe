61a7a63f60f65a7c5370f2d055ea63ddad03d2e9
William Patruno <wpatruno@immersivefactory.com>
2018-01-28 14:56:02 +0100

    Update game over eye

---
+++ TheTribe/Assets/GodEyeGameOver.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GodEyeGameOver : MonoBehaviour {
+	public Sprite eyeDownLeft;
+	public Sprite eyeDown;
+	public Sprite eyeIdle;
+	public Sprite eyeLeft;
+	public Sprite eyeLove;
+	public Sprite eyeAngry;
+	public Sprite eyeClosed;
+	SpriteRenderer eyeRenderer;
+	private void Start()
+	{
+		eyeRenderer = GetComponent<SpriteRenderer>();
+
+	}
+
+
+	void Update () {
+		//Debug.Log(Input.mousePosition);
+		if(Input.mousePosition.y > (Screen.height - 200))
+		{
+			
+			if (Input.mousePosition.x < (Screen.width / 2) - 100)
+			{
+				eyeRenderer.sprite = eyeLeft;
+				eyeRenderer.flipX = false;
+			}
+			else if (Input.mousePosition.x > (Screen.width / 2) + 100)
+			{
+				eyeRenderer.sprite = eyeLeft;
+				eyeRenderer.flipX = true;
+			}
+			else
+			{
+				if(Input.GetMouseButton(0))
+				{
+					eyeRenderer.sprite = eyeClosed;
+				}
+				else if (Input.GetMouseButton(1))
+				{
+					eyeRenderer.sprite = eyeClosed;
+				}
+				else
+				{
+					eyeRenderer.sprite = eyeIdle;
+				}
+			}
+		}
+		else
+		{
+			if(Input.mousePosition.x < (Screen.width/2) - 100)
+			{
+				eyeRenderer.flipX = false;
+				eyeRenderer.sprite = eyeDownLeft;
+			}
+			else if (Input.mousePosition.x > (Screen.width / 2) + 100)
+			{
+				eyeRenderer.flipX = true ;
+				eyeRenderer.sprite = eyeDownLeft;
+			}
+			else
+			{
+				eyeRenderer.sprite = eyeDown;
+			}
+		}
+	}
+
+
+}
