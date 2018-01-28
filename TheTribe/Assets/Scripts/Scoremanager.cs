39cbd1df77d459aab598c9f40c4158908d147d5e
Olivier <ochabiron@immersivefactory.com>
2018-01-28 18:32:19 +0100

    FIX LOL

--- TheTribe/Assets/Scripts/Scoremanager.cs
+++ TheTribe/Assets/Scripts/Scoremanager.cs
@@ -70,19 +70,27 @@ public class ScoreManager : MonoBehaviour
         gp.generatedAspect.godAspectSprite = acceptedPart.totemAspectSprite;
         int age = TribeManager.instance.GetAge();
 
+        TotemManager.generatedGodAspect godAspect;
+        godAspect = totemManager.GetGodHeadGeneratedAspect();
+
         switch (age)
         {
             case 0:
                 gp.name = "Head";
+                // Compare part with ideal
+                godAspect = totemManager.GetGodHeadGeneratedAspect();
                 break;
             case 1:
                 gp.name = "UpperBody";
+                godAspect = totemManager.GetGodUpperbodyGeneratedAspect();
                 break;
             case 2:
                 gp.name = "LowerBody";
+                godAspect = totemManager.GetGodLowerbodyGeneratedAspect();
                 break;
             case 3:
                 gp.name = "Accessory";
+                godAspect = totemManager.GetGodAccessoryGeneratedAspect();
                 break;
         }
         
@@ -90,12 +98,16 @@ public class ScoreManager : MonoBehaviour
         savedTotem.Add(gp);
         SceneManager.instance.savedTotem = savedTotem;
 
-        // Compare part with ideal
-        TotemManager.generatedGodAspect godAspect = totemManager.GetGodHeadGeneratedAspect();
-
+        Debug.Log(score.ToString() + "avant modif");
         score += godAspect.category == acceptedPart.category ? 1 : 0;
+
+        Debug.Log(score.ToString() + "category god="+ godAspect.category.ToString() + ", totem="+ acceptedPart.category.ToString());
         score += godAspect.isAspectPositive == acceptedPart.isAspectPositive ? 1 : 0;
+        Debug.Log(score.ToString() + "alignment god=" + godAspect.isAspectPositive.ToString() + ", totem=" + acceptedPart.isAspectPositive.ToString());
         score += godAspect.aspectName == acceptedPart.aspectName ? 1 : 0;
+        Debug.Log(score.ToString() + "aspect god=" + godAspect.aspectName.ToString() + ", totem=" + acceptedPart.aspectName.ToString());
+
+        //Debug.Log(score.ToString());
 
         // Update Score
         UpdateScore();
