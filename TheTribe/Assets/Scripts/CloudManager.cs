030d0fd486b238a47ff5c572af725f224dd1686b
William Patruno <wpatruno@immersivefactory.com>
2018-01-28 13:23:40 +0100

    Add god in clouds

--- TheTribe/Assets/Scripts/CloudManager.cs
+++ TheTribe/Assets/Scripts/CloudManager.cs
@@ -10,34 +10,34 @@ public class CloudManager : MonoBehaviour {
 	public Image background;
 	float moveDuration = 4;
 	float moveSpeed = 3f;
+	float godMoveSpeed = 3f;
 	static CloudManager instance;
 	bool wallDisabled = false;
 	public UnityEvent OnFirstWallGone;
 	public UnityEvent OnSkyExit;
-
+	Transform godSprites;
+	SpriteRenderer godRay;
+	Coroutine godBlink;
 	void Awake () {
 		instance = this;
 		CloudWallHolder = transform.Find("CloudWall");
 		CloudSkyHolder = transform.Find("CloudSky");
-
+		godSprites = transform.Find("God");
+		godRay = transform.Find("GodRay").GetComponent<SpriteRenderer>();
+		godRay.color = new Color(1,1,1,0);
 		// For Debug
-		ExitSky();
+		MoveCloudWall();
 	}
 	
-	public static void MoveCloudWall()
+	public void MoveCloudWall()
 	{
-		if(instance)
-		{
-			instance.StartCoroutine(instance.HideCloudsWall());
-		}
+		StartCoroutine(instance.HideCloudsWall());
 	}
 
-	public static void ExitSky()
+	public void ExitSky()
 	{
-		if (instance)
-		{
-			instance.StartCoroutine(instance.HideCloudsSky());
-		}
+
+		StartCoroutine(instance.HideCloudsSky());
 	}
 
 	protected IEnumerator HideCloudsSky()
@@ -47,6 +47,7 @@ public class CloudManager : MonoBehaviour {
 		{
 			yield return StartCoroutine(HideCloudsWall());
 		}
+		if(godBlink != null) StopCoroutine(godBlink);
 		float startTime = 0;
 		float percentage = 0;
 		Color color = CloudSkyHolder.GetChild(0).GetComponent<Image>().color;
@@ -60,18 +61,19 @@ public class CloudManager : MonoBehaviour {
 			background.color = back;
 			foreach (Transform cloud in CloudSkyHolder)
 			{
-				if (cloud.transform.position.x < Screen.width / 2)
+				if (cloud.transform.position.x < Camera.main.transform.position.x)
 				{
-					cloud.transform.position = cloud.transform.position + Vector3.left * moveSpeed;
+					cloud.transform.position = cloud.transform.position + Vector3.left * moveSpeed * Time.deltaTime;
 				}
 				else
 				{
-					cloud.transform.position = cloud.transform.position + Vector3.right * moveSpeed;
+					cloud.transform.position = cloud.transform.position + Vector3.right * moveSpeed * Time.deltaTime;
 				}
 				cloud.gameObject.GetComponent<Image>().color = color;
 			}
-			
-			yield return 0;
+			godRay.color = new Color(1, 1, 1, back.a);
+			godSprites.transform.position = godSprites.transform.position + Vector3.up * godMoveSpeed * Time.deltaTime;
+			yield return new WaitForEndOfFrame();
 		}
 		OnSkyExit.Invoke();
 		yield return new WaitForSeconds(0.3f);
@@ -91,25 +93,50 @@ public class CloudManager : MonoBehaviour {
 
 	protected IEnumerator HideCloudsWall()
 	{
+		wallDisabled = true;
 		yield return new WaitForSeconds(1);
 		float startTime = 0;
-		while(startTime < moveDuration)
+		while (startTime < moveDuration)
 		{
 			startTime += Time.deltaTime;
 			foreach (Transform cloud in CloudWallHolder)
 			{
-				if(cloud.transform.position.x < Screen.width / 2)
+				if(cloud.transform.position.x < Camera.main.transform.position.x)//Screen.width / 2)
 				{
-					cloud.transform.position = cloud.transform.position + Vector3.left * moveSpeed;
+					cloud.transform.position = cloud.transform.position + Vector3.left * moveSpeed * Time.deltaTime;
 				}
 				else
 				{
-					cloud.transform.position = cloud.transform.position + Vector3.right * moveSpeed;
+					cloud.transform.position = cloud.transform.position + Vector3.right * moveSpeed * Time.deltaTime;
 				}
 			}
-			yield return 0;
+			godRay.color = new Color(1, 1, 1, godRay.color.a + Time.deltaTime);
+			yield return new WaitForEndOfFrame();
 		}
-		wallDisabled = true;
+		godBlink = StartCoroutine(BlinkGodRay());
 		OnFirstWallGone.Invoke();
 	}
+
+	IEnumerator BlinkGodRay()
+	{
+		Color color = godRay.color;
+		color.a = 1;
+		while (true)
+		{
+			while(color.a > 0.5f)
+			{
+				color.a -= Time.deltaTime * 0.3f;
+				godRay.color = color;
+				yield return new WaitForEndOfFrame();
+			}
+
+			while (color.a < 1f)
+			{
+				color.a += Time.deltaTime * 0.3f;
+				godRay.color = color;
+				yield return new WaitForEndOfFrame();
+			}
+		}
+	}
+	
 }
